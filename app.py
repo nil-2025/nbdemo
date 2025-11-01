84170157317f61cd45bf999c78a242364b75795c
nil-2025 <nilotpal.bhuniya@gmail.com>
2025-11-01 17:57:58 +0530

    new commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello, Flask is working!</h1>"
+    return "<h1>Hello, Flask is working fine!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
